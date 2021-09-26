35395e6d67625abc166101fbc2e14730f4561265
kbanchi1111 <kbanchi.1111@gmail.com>
2021-09-26 18:20:03 +0900

    update map

--- resources/views/partial/map.blade.php
+++ resources/views/partial/map.blade.php
@@ -6,7 +6,7 @@
 integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
 crossorigin=""></script>
 
-<script>
+{{-- <script>
     // 地図描画エリアを作成
     const map = L.map('map');
 
@@ -16,4 +16,40 @@ crossorigin=""></script>
 
     // 表示するタイルを指定
     L.tileLayer('http://tile.openstreetmap.jp/{z}/{x}/{y}.png').addTo(map);
+</script> --}}
+<script>
+    //地図描画 オプションでzoomControlをfalseにするとzoomControlを消せる(下記で別な場所に表示)
+    var map = L.map('map');
+
+    // 中心とzoomを指定
+    map.setView([{{ $latitude }}, {{ $longitude }}], {{ $zoom }});
+
+    // スケール表示 (imperial: falseにしないとmile表示も出る)
+    L.control.scale({
+        maxWidth: 200,
+        position: 'bottomright',
+        imperial: false
+    }).addTo(map);
+
+    var baseMaps = {
+        "地理院地図": L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+            attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+        }),
+        "淡色地図": L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+            attribution: "<a href='http://portal.cyberjapan.jp/help/termsofuse.html' target='_blank'>地理院タイル</a>"
+        }),
+        "オープンストリートマップ": L.tileLayer('http://tile.openstreetmap.jp/{z}/{x}/{y}.png', {
+            attribution: "<a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors"
+        }),
+        "衛生写真": L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
+            attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+        }),
+    };
+
+    //layersコントロールにbaseMapsオブジェクトを設定して地図に追加
+    //コントロール内にプロパティ名が表示される
+    L.control.layers(baseMaps).addTo(map);
+
+    //最初に表示するタイルを指定
+    baseMaps['オープンストリートマップ'].addTo(map);
 </script>
